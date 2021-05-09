dff8ff93e2c50d6de1d6d4ac447fa5918c2c0dd8
ElliotWhiley <elliot.whiley94@gmail.com>
2021-05-09 23:49:24 +1200

    Add checks for invalid input and substitution values

--- Core/TextHelpers.cs
+++ Core/TextHelpers.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -9,15 +10,19 @@ namespace Core
         // Only captures values enclosed by an odd number of delimiters, otherwise an escaped character is implied
         static readonly Regex InterpolatedStringSubstitutionPattern = new Regex(@"(?<!\[)(\[\[)*\[(?!\[).*?\]", RegexOptions.IgnoreCase);
 
-        public static string Interpolate(string input, Dictionary<string, string> interpolationValues)
+        public static string Interpolate(string input, Dictionary<string, string> substitutionValues)
         {
+            if (substitutionValues == null) throw new ArgumentException("Substitution values must not be null.", nameof(substitutionValues));
+            if (substitutionValues.Count == 0) throw new ArgumentException("Substitution values must not be empty.", nameof(substitutionValues));
+
             var interpolationPlaceholders = InterpolatedStringSubstitutionPattern.Matches(input);
+            if (interpolationPlaceholders.Count > substitutionValues.Count) throw new ArgumentException("More placeholders than substitution values provided.", nameof(input));
 
             foreach (var placeholder in interpolationPlaceholders) {
                 var interpolationPlaceholder = placeholder.ToString();
                 var numberOfDelimiters = interpolationPlaceholder.Count(x => x == '[');
                 var interpolationPlaceholderKey = interpolationPlaceholder.Replace("[", "").Replace("]", "");
-                input = input.Replace("[" + interpolationPlaceholderKey + "]", interpolationValues[interpolationPlaceholderKey]); //try get value
+                input = input.Replace("[" + interpolationPlaceholderKey + "]", substitutionValues[interpolationPlaceholderKey]); //try get value
             }
 
             input = input.Replace("[[", "[");
