a8174b9d9f240cd3aad2794d537d8ca8a258659b
ElliotWhiley <elliot.whiley94@gmail.com>
2021-05-10 00:05:14 +1200

    Add implementation to ensure placeholder has valid substitution mapping in provided dictionary

--- Core/TextHelpers.cs
+++ Core/TextHelpers.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Core
@@ -12,8 +11,8 @@ namespace Core
 
         public static string Interpolate(string input, Dictionary<string, string> substitutionValues)
         {
-            if (substitutionValues == null) throw new ArgumentException("Substitution values must not be null.", nameof(substitutionValues));
-            if (substitutionValues.Count == 0) throw new ArgumentException("Substitution values must not be empty.", nameof(substitutionValues));
+            if (substitutionValues == null) throw new ArgumentException("Substitution values must not be null", nameof(substitutionValues));
+            if (substitutionValues.Count == 0) throw new ArgumentException("Substitution values must not be empty", nameof(substitutionValues));
 
             var interpolationPlaceholders = InterpolatedStringSubstitutionPattern.Matches(input);
             if (interpolationPlaceholders.Count > substitutionValues.Count) throw new ArgumentException("More placeholders than substitution values provided.", nameof(input));
@@ -21,7 +20,10 @@ namespace Core
             foreach (var placeholder in interpolationPlaceholders) {
                 var interpolationPlaceholder = placeholder.ToString();
                 var interpolationPlaceholderKey = interpolationPlaceholder.Replace("[", "").Replace("]", "");
-                input = input.Replace("[" + interpolationPlaceholderKey + "]", substitutionValues[interpolationPlaceholderKey]); //try get value
+                var foundMatchingValue = substitutionValues.TryGetValue(interpolationPlaceholderKey, out var interpolationPlaceholderValue);
+                if (!foundMatchingValue) throw new ArgumentException("Substitution value not found for placeholder: " + interpolationPlaceholderKey, nameof(substitutionValues));
+
+                input = input.Replace("[" + interpolationPlaceholderKey + "]", interpolationPlaceholderValue);
             }
 
             input = input.Replace("[[", "[");
